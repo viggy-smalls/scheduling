b8113648f57928af53f46277db75086f01559bad
Matthew Hunt <matthew.hunt01@gmail.com>
2014-11-30 18:43:38 -0600

    updated add_to_waiting

--- student.c
+++ student.c
@@ -24,6 +24,7 @@
  * for your use.
  */
 static pcb_t **current; 
+static pcb_t *waiting;
 static pthread_mutex_t current_mutex;
 static pthread_mutex_t ready_mutex;
 
@@ -95,28 +96,11 @@ extern void idle(unsigned int cpu_id)
     mt_safe_usleep(1000000);
 }
 
-static void add_to_waiting(pcb_t *pcb, unsigned int execution_time)
+static void add_to_waiting(pcb_t *pcb, pcb_t *new)
 {
-    io_request *r;
-
-    /* Build I/O Request */
-    r = malloc(sizeof(io_request));
-    assert(r != NULL);
-    r->pcb = pcb;
-    r->execution_time = execution_time;
-    r->next = NULL;
-
-    /* Add request to head of queue */
-    if (io_queue_tail != NULL)
-    {
-        io_queue_tail->next = r;
-        io_queue_tail = r;
-    }
-    else
-    {
-        io_queue_head = r;
-        io_queue_tail = r;
-    }
+    if (pcb->next == NULL)
+		pcb->next = new;
+	else add_to_waiting(pcb->next, new);
 }
 /*
  * preempt() is the handler called by the simulator when a process is
