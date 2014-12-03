07103978feea5d89f3c090c98f8249b9ffa213ea
viggy-smalls <Vignesh.sekar770@topper.WKU.edu>
2014-12-02 21:34:54 -0600

    16

--- student.c
+++ student.c
@@ -30,7 +30,7 @@ static pcb_t *ready;
 static pthread_mutex_t current_mutex;
 static pthread_mutex_t ready_mutex;
 static pthread_cond_t ready_cond;
-static int waiting[8];
+static int waiting[16];
 static int rr;
 static int preempt_time;
 
